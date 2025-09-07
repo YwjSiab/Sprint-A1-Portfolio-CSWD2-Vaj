53fdf6dfd384e1fd0903fc871b1c4c116f391e13
Ywj Siab Vaj <yvaj@students.ntc.edu>
2025-09-07 18:33:51 -0500

    Update NavSidebar.js

--- components/NavSidebar.js
+++ components/NavSidebar.js
@@ -1,21 +1,22 @@
+// components/NavSidebar.js
 class NavSidebar extends HTMLElement {
-  constructor() {
-    super();
-    const r = this.attachShadow({mode:"open"});
-    r.innerHTML = `
-      <aside>
+  connectedCallback() {
+    // Light DOM (no Shadow DOM) so page CSS can style it
+    this.innerHTML = `
+      <div class="sidebar-box">
         <nav>
+          <h2> 
+            <span class="asideText">Links to My</span>
+          </h2>
           <ul>
-            <h2 class="asideText">Links to My</h2>
             <li><a href="index.html"><strong>Home</strong></a></li>
             <li><a href="Resume.html">Resume</a></li>
             <li><a href="Projects.html">Projects</a></li>
             <li><a href="Contact.html">Contact</a></li>
           </ul>
         </nav>
-      </aside>`;
+      </div>
+    `;
   }
 }
 customElements.define("nav-sidebar", NavSidebar);
-
-
